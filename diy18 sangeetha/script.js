0ae397ddd3b14b2b3ed25ee16a4660b90b84c1b5
steffysangeetha <144100657+steffysangeetha@users.noreply.github.com>
2023-09-05 12:49:41 +0530

    Add files via upload

---
+++ diy18 sangeetha/script.js
@@ -0,0 +1,41 @@
+let a= 50;
+let b= 10;
+console.log(a+b);  
+console.log(a-b);  
+console.log(a*b);  
+console.log(a/b);  
+console.log(a%b);  
+
+x = 'hello';
+y = 'welcome';
+console.log(x==y);
+
+y = 'hello';
+console.log(x==y);
+
+x = 10;
+y = '10';
+console.log(x==y);
+console.log(x===y);
+console.log(x==y);
+console.log(x!==y);
+
+
+y = 10;
+console.log(x===y);
+console.log(x<y);
+console.log(x>y);
+console.log(x<=y);
+console.log(x>=y);
+console.log(x!=y);
+
+
+x=543; y =345;
+console.log(x,y);
+
+x += 5;
+console.log(x); 
+console.log(x&&y); 
+console.log(x||y); 
+console.log(x===y?'welcome':'world'); 
+
