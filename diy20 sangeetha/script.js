1346e5197f7763a8072619417c49efa2c1a9aa5a
steffysangeetha <144100657+steffysangeetha@users.noreply.github.com>
2023-09-05 12:50:39 +0530

    Add files via upload

---
+++ diy20 sangeetha/script.js
@@ -0,0 +1,19 @@
+const newstring="let's slice the new string";
+const slicestring=newstring.slice(6,11);
+console.log(newstring);
+
+
+let text="today is a dummy day.";
+const newtext=text.replace('dummy','great');
+console.log(newtext);
+
+let capitalText= newtext.toUpperCase();
+console.log(capitalText);
+
+let concattext=newtext.concat('welcome to edureka');
+console.log(concattext);
+
+let newtext2="lorem ipsem dollor..........";
+console.log(newtext2);
+
+
